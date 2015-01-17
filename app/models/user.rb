4b5c3a6c12d3e8f4c79e8ed16c185f877c0dcb82
Sam Farmer <sam_farmer@hotmail.com>
2015-01-17 12:03:38 +0000

    Up to add secure password in chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
 
+  before_save :downcase_email
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
 
 
-
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
 end
