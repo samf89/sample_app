e4107a51fd26899a8bdd3e7dde6abc2c5bcf80f9
Sam <samuel.farmer@intercity-uk.com>
2015-01-30 10:09:59 +0000

    upto the image section of chapter 11

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -20,5 +20,13 @@ class MicropostsControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
+  test "should redirect destroy action for wrong micropost" do
+    log_in_as(users(:test_user_one))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: micropost
+    end
+    assert_redirected_to root_url
+  end
 
 end
