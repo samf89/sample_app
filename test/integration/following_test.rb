0c23f3466ef3166f8d40cb7adc3103ef64f3736c
Sam Farmer <sam_farmer@hotmail.com>
2015-02-01 16:59:43 +0000

    up to the status feed part of chapter 12

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:test_user_one)
+    @other_user = users(:archer)
     log_in_as(@user)
   end
 
@@ -24,4 +25,33 @@ class FollowingTest < ActionDispatch::IntegrationTest
       assert_select "a[href=?]", user_path(user)
     end
   end
+
+  test "follow user with html request" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, followed_id: @other_user.id
+    end
+  end
+
+  test "unfollow user with html request" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "follow with ajax request" do 
+    assert_difference '@user.following.count', 1  do 
+      xhr :post, relationships_path, followed_id: @other_user.id
+    end
+  end
+
+  test "unfollow with ajax request" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user)
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship)
+    end
+  end
+
 end
